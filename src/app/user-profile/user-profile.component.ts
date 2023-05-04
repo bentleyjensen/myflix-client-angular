4343f7dcb58e0509380d781371c4cb192bdf7b05
bentleyjensen <bentleyjensen@users.noreply.github.com>
2023-05-03 21:35:28 -0700

    create user profile page mvp

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { FetchApiDataService } from "../fetch-api-data.service";
+
+type User = {
+  username: string;
+  email: string;
+  birthdate: Date | string;
+  favorites: any[];
+  password: string;
+  confirmPassword: string;
+};
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit{
+  @Input() userDetails: User = {
+    username: '',
+    email: '',
+    birthdate: '',
+    password: '',
+    confirmPassword: '',
+    favorites: [],
+  }
+
+  constructor (
+    public fetchApiDataService: FetchApiDataService,
+    public snackBar: MatSnackBar,
+  ){}
+
+  ngOnInit(): void {
+    this.fetchUser();
+  }
+
+  public fetchUser(): void{
+    const myUser: User = {
+      username: 'Bentimus',
+      email: 'bentimus@maximus.com',
+      birthdate: '1996-07-19',
+      password: '',
+      confirmPassword: '',
+      favorites: [
+        {
+          _id: '6293196ff04c1332bdae9494',
+          title: 'Inception',
+        },{
+          _id: '6293196ff04c1332bdae9495',
+          title: 'Batman Begins',
+        },{
+          _id: '6293196ff04c1332bdae9493',
+          title: 'Interstellar',
+        },
+      ],
+    }
+
+    this.userDetails = myUser;
+  }
+
+  public deleteFavorite(): void {
+
+  }
+
+  public updateUser(): void {
+
+  }
+
+  public deleteUser(): void {
+
+  }
+}
